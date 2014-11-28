2a558efcc6c51b79ed10d9f3788fcad5fb4de23d
victor <victor@notwebdesign.com>
2014-11-28 12:58:29 +0100

    Adding settings table.

--- administrator/components/com_lingo/sql/uninstall.mysql.utf8.sql
+++ administrator/components/com_lingo/sql/uninstall.mysql.utf8.sql
@@ -1,2 +1,5 @@
 DROP TABLE IF EXISTS `#__lingo_langfile_translations`;
 DROP TABLE IF EXISTS `#__lingo_langfile_source`;
+DROP TABLE IF EXISTS `#__lingo_manifest_tables`;
+DROP TABLE IF EXISTS `#__lingo_manifest_fields`;
+DROP TABLE IF EXISTS `#__lingo_settings`;
