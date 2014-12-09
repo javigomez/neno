2ccf036378cf6989d48086e7dc06eb1e02c246e2
Victor Escobar González <victor@notwebdesign.com>
2014-12-09 12:54:18 +0100

    FEATURE Creating class structure for translators.

--- libraries/neno/translate/factory.php
+++ libraries/neno/translate/factory.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package    Neno
- * @subpackage
+ * @package     Neno
+ * @subpackage  Translate
  *
- * @copyright  Copyright (c) 2014 Jensen Technologies S.L. All rights reserved
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright   Copyright (c) 2014 Jensen Technologies S.L. All rights reserved
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('JPATH_NENO') or die;
 
@@ -13,7 +13,41 @@ defined('JPATH_NENO') or die;
  *
  * @since  1.0
  */
-class NenoTranslatorFactory
+class NenoTranslateFactory
 {
+	/**
+	 * @var array
+	 */
+	private static $translators = array();
 
+	/**
+	 * Get a translator
+	 *
+	 * @param   string  $name  Translator Name
+	 *
+	 * @return null|
+	 */
+	public static function getTranslator($name)
+	{
+		if (!empty($name))
+		{
+			$className = 'NenoTranslateTranslator' . ucfirst(strtolower($name));
+
+			$translatorSignature = md5($className);
+
+			if (!isset(self::$translators[$translatorSignature]))
+			{
+				if (class_exists($className))
+				{
+					$translator = new $className;
+
+					self::$translators[$translatorSignature] = $translator;
+				}
+
+				return null;
+			}
+
+			return self::$translators[$translatorSignature];
+		}
+	}
 }
--- libraries/neno/translate/translator/adapter.php
+++ libraries/neno/translate/translator/adapter.php
@@ -8,7 +8,34 @@
  */
 defined('JPATH_NENO') or die;
 
-class NenoTranslateTranslatorAdapter
+/**
+ * Class NenoTranslateTranslatorAdapter
+ *
+ * @since  1.0
+ */
+abstract class NenoTranslateTranslatorAdapter
 {
+	/**
+	 * @var string
+	 */
+	private $name;
 
-} 
+	/**
+	 * Constructor
+	 *
+	 * @param   string  $name  Translator Name
+	 */
+	public function __construct($name)
+	{
+		$this->name = $name;
+	}
+
+	/**
+	 * Method to translate a certain string
+	 *
+	 * @param   string  $string  String to translate
+	 *
+	 * @return void
+	 */
+	public abstract function translate($string);
+}
