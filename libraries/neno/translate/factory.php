f9f7594ce606928721e562f6085eac66978cf426
Victor Escobar González <victor@notwebdesign.com>
2014-12-09 12:53:26 +0100

    FEATURE Creating class structure for translators.

---
+++ libraries/neno/translate/factory.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package    Neno
+ * @subpackage
+ *
+ * @copyright  Copyright (c) 2014 Jensen Technologies S.L. All rights reserved
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('JPATH_NENO') or die;
+
+/**
+ * Class NenoTranslatorFactory
+ *
+ * @since  1.0
+ */
+class NenoTranslatorFactory
+{
+
+}
---
+++ libraries/neno/translate/translator/adapter.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package     Neno
+ * @subpackage  Translate
+ *
+ * @copyright   Copyright (c) 2014 Jensen Technologies S.L. All rights reserved
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('JPATH_NENO') or die;
+
+class NenoTranslateTranslatorAdapter
+{
+
+} 
