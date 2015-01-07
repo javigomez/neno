d25b4bcf8599c6b4e60ae2cbd20a59ffc22e7671
Victor Escobar González <victor@notwebdesign.com>
2015-01-07 11:22:57 +0100

    DOCUMENTATION Improve documentation of the content element base class.

--- libraries/neno/content/element.php
+++ libraries/neno/content/element.php
@@ -47,7 +47,76 @@ abstract class NenoContentElement
 		}
 	}
 
+	/**
+	 * Get a ReflectionObject to work with it.
+	 *
+	 * @return ReflectionClass
+	 */
 	public abstract function getClassReflectionObject();
+
+	/**
+	 * Method to persist object in the database
+	 *
+	 * @return boolean
+	 */
+	public function persist()
+	{
+		$db   = JFactory::getDbo();
+		$data = $this->toObject();
+
+		if ($this->isNew())
+		{
+			$result   = $db->insertObject($this->getDbTable(), $data, 'id');
+			$this->id = $db->insertid();
+		}
+		else
+		{
+			$result = $db->updateObject($this->getDbTable(), $data, 'id');
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Create a JObject using the properties of the class.
+	 *
+	 * @return JObject
+	 */
+	public function toObject()
+	{
+		$data = new JObject;
+
+		// Create a reflection class to use it to dynamic properties loading
+		$classReflection = $this->getClassReflectionObject();
+
+		// Getting all the properties marked as 'protected'
+		$properties = $classReflection->getProperties(ReflectionProperty::IS_PROTECTED);
+
+		// Go through them and assign a value to them if they exist in the argument passed as parameter.
+		foreach ($properties as $property)
+		{
+			$data->set(NenoHelper::convertPropertyNameToDatabaseColumnName($property->getName()), $this->{$property->getName()});
+		}
+
+		return $data;
+	}
+
+	/**
+	 * Check if the object is new
+	 *
+	 * @return bool
+	 */
+	public function isNew()
+	{
+		return empty($this->id);
+	}
+
+	/**
+	 * Get the name of the database to persist the object
+	 *
+	 * @return string
+	 */
+	public abstract function getDbTable();
 
 	/**
 	 * Id getter
