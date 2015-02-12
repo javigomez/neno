75bb0834489f971188f40e50b93106128ac7c99f
Victor Escobar González <victor@notwebdesign.com>
2015-02-12 10:07:04 +0100

    BUGFIX Updating SQL installation file.

--- administrator/components/com_neno/sql/install.mysql.utf8.sql
+++ administrator/components/com_neno/sql/install.mysql.utf8.sql
@@ -1,7 +1,5 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `#__neno_content_elements_metadata_x_translators`
@@ -10,10 +8,7 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `#__neno_content_elements_metadata_x_translators` (
   `translator_id` int(11) NOT NULL,
   `content_element_metadata_id` int(11) NOT NULL,
-  `state` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`translator_id`,`content_element_metadata_id`),
-  KEY `cem_x_translators_translator_idx` (`translator_id`),
-  KEY `cem_x_translators_content_element_metadata_idx` (`content_element_metadata_id`)
+  `state` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -23,14 +18,25 @@ CREATE TABLE IF NOT EXISTS `#__neno_content_elements_metadata_x_translators` (
 --
 
 CREATE TABLE IF NOT EXISTS `#__neno_content_element_fields` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `table_id` int(11) NOT NULL,
   `field_name` varchar(100) NOT NULL,
-  `translate` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `table_id_x_field` (`table_id`,`field_name`),
-  KEY `content_elements_fields_table_idx` (`table_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2823 ;
+  `field_type` varchar(45) NOT NULL,
+  `type` varchar(45) NOT NULL,
+  `translate` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `#__neno_content_element_fields_x_translations`
+--
+
+CREATE TABLE IF NOT EXISTS `#__neno_content_element_fields_x_translations` (
+  `field_id` int(11) NOT NULL,
+  `translation_id` int(11) NOT NULL,
+  `value` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -39,22 +45,19 @@ CREATE TABLE IF NOT EXISTS `#__neno_content_element_fields` (
 --
 
 CREATE TABLE IF NOT EXISTS `#__neno_content_element_groups` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `group_name` varchar(150) NOT NULL,
-  `extension_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `group_name_UNIQUE` (`group_name`),
-  UNIQUE KEY `extension_id_unique` (`extension_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=337 ;
+  `extension_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `#__neno_content_element_langfile_sources`
+-- Table structure for table `#__neno_content_element_langstrings`
 --
 
-CREATE TABLE IF NOT EXISTS `#__neno_content_element_langfile_sources` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `#__neno_content_element_langstrings` (
+  `id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL,
   `constant` varchar(255) NOT NULL,
   `string` text NOT NULL,
@@ -64,69 +67,21 @@ CREATE TABLE IF NOT EXISTS `#__neno_content_element_langfile_sources` (
   `time_changed` datetime NOT NULL,
   `time_deleted` datetime NOT NULL,
   `state` tinyint(1) NOT NULL,
-  `version` tinyint(4) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `constant` (`constant`,`language`),
-  KEY `state` (`state`),
-  KEY `extension` (`extension`),
-  KEY `language` (`language`),
-  KEY `group_id` (`group_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=80590 ;
+  `version` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `#__neno_content_element_langfile_translations`
+-- Table structure for table `#__neno_content_element_presets`
 --
 
-CREATE TABLE IF NOT EXISTS `#__neno_content_element_langfile_translations` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `source_id` int(11) NOT NULL,
-  `language` varchar(5) NOT NULL DEFAULT '',
-  `string` text NOT NULL,
-  `time_translated` datetime NOT NULL,
-  `time_deleted` datetime NOT NULL,
-  `version` tinyint(4) NOT NULL,
-  `translation_method` enum('langfile','machine','manual','pro') NOT NULL,
-  `state` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `langfile_translations_source_idx` (`source_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6256 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `#__neno_content_element_metadata`
---
-
-CREATE TABLE IF NOT EXISTS `#__neno_content_element_metadata` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `content_type` enum('lang_string','db_string') NOT NULL,
-  `content_id` int(11) NOT NULL,
-  `lang` varchar(5) NOT NULL,
-  `state` tinyint(1) NOT NULL,
-  `string` text NOT NULL,
-  `time_added` datetime NOT NULL,
-  `time_requested` datetime NOT NULL,
-  `time_completed` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `#__neno_content_element_preset`
---
-
-CREATE TABLE IF NOT EXISTS `#__neno_content_element_preset` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `#__neno_content_element_presets` (
+  `id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL,
   `table_id` int(11) NOT NULL,
-  `lang` varchar(5) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `group_id_x_table_idx` (`group_id`,`table_id`,`lang`),
-  KEY `content_elements_preset_group_idx` (`group_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `lang` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -137,10 +92,7 @@ CREATE TABLE IF NOT EXISTS `#__neno_content_element_preset` (
 CREATE TABLE IF NOT EXISTS `#__neno_content_element_preset_x_translators` (
   `translator_id` int(11) NOT NULL,
   `content_element_preset_id` int(11) NOT NULL,
-  `state` tinyint(1) NOT NULL,
-  PRIMARY KEY (`translator_id`,`content_element_preset_id`),
-  KEY `fk_cep_x_translators_content_elements_preset_idx` (`content_element_preset_id`),
-  KEY `fk_cep_x_translators_translator_idx` (`translator_id`)
+  `state` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -150,16 +102,32 @@ CREATE TABLE IF NOT EXISTS `#__neno_content_element_preset_x_translators` (
 --
 
 CREATE TABLE IF NOT EXISTS `#__neno_content_element_tables` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL,
   `table_name` varchar(255) NOT NULL,
-  `primary_key` varchar(5) NOT NULL,
-  `translate` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `group_id_x_table_name` (`group_id`,`table_name`),
-  UNIQUE KEY `table_name` (`table_name`),
-  KEY `content_elements_tables_group_idx` (`group_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1058 ;
+  `primary_key` varchar(255) NOT NULL,
+  `translate` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `#__neno_content_element_translations`
+--
+
+CREATE TABLE IF NOT EXISTS `#__neno_content_element_translations` (
+  `id` int(11) NOT NULL,
+  `content_type` enum('lang_string','db_string') NOT NULL,
+  `content_id` int(11) NOT NULL,
+  `language` varchar(5) NOT NULL,
+  `state` tinyint(1) NOT NULL,
+  `string` text NOT NULL,
+  `time_added` datetime NOT NULL,
+  `time_requested` datetime NOT NULL,
+  `time_completed` datetime NOT NULL,
+  `translation_method` enum('machine','manual','pro','') NOT NULL,
+  `version` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -168,26 +136,115 @@ CREATE TABLE IF NOT EXISTS `#__neno_content_element_tables` (
 --
 
 CREATE TABLE IF NOT EXISTS `#__neno_settings` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `setting_key` varchar(150) NOT NULL,
   `setting_value` varchar(255) NOT NULL,
-  `read_only` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `setting_key_UNIQUE` (`setting_key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+  `read_only` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Table structure for table `#__neno_translators`
+-- Indexes for dumped tables
 --
 
-CREATE TABLE IF NOT EXISTS `#__neno_translators` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `translator_name` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+--
+-- Indexes for table `#__neno_content_elements_metadata_x_translators`
+--
+ALTER TABLE `#__neno_content_elements_metadata_x_translators`
+ADD PRIMARY KEY (`translator_id`,`content_element_metadata_id`), ADD KEY `cem_x_translators_translator_idx` (`translator_id`), ADD KEY `cem_x_translators_content_element_metadata_idx` (`content_element_metadata_id`);
 
+--
+-- Indexes for table `#__neno_content_element_fields`
+--
+ALTER TABLE `#__neno_content_element_fields`
+ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `table_id_x_field` (`table_id`,`field_name`), ADD KEY `content_elements_fields_table_idx` (`table_id`);
+
+--
+-- Indexes for table `#__neno_content_element_fields_x_translations`
+--
+ALTER TABLE `#__neno_content_element_fields_x_translations`
+ADD PRIMARY KEY (`field_id`,`translation_id`), ADD KEY `fk_#__neno_content_element_fields_idx` (`translation_id`), ADD KEY `fk_#__neno_content_element_fields_idx1` (`field_id`);
+
+--
+-- Indexes for table `#__neno_content_element_groups`
+--
+ALTER TABLE `#__neno_content_element_groups`
+ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `group_name_UNIQUE` (`group_name`), ADD UNIQUE KEY `extension_id_unique` (`extension_id`);
+
+--
+-- Indexes for table `#__neno_content_element_langstrings`
+--
+ALTER TABLE `#__neno_content_element_langstrings`
+ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `constant` (`constant`,`language`), ADD KEY `state` (`state`), ADD KEY `extension` (`extension`), ADD KEY `language` (`language`), ADD KEY `group_id` (`group_id`);
+
+--
+-- Indexes for table `#__neno_content_element_presets`
+--
+ALTER TABLE `#__neno_content_element_presets`
+ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `group_id_x_table_idx` (`group_id`,`table_id`,`lang`), ADD KEY `content_elements_preset_group_idx` (`group_id`);
+
+--
+-- Indexes for table `#__neno_content_element_preset_x_translators`
+--
+ALTER TABLE `#__neno_content_element_preset_x_translators`
+ADD PRIMARY KEY (`translator_id`,`content_element_preset_id`), ADD KEY `fk_cep_x_translators_content_elements_preset_idx` (`content_element_preset_id`), ADD KEY `fk_cep_x_translators_translator_idx` (`translator_id`);
+
+--
+-- Indexes for table `#__neno_content_element_tables`
+--
+ALTER TABLE `#__neno_content_element_tables`
+ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `group_id_x_table_name` (`group_id`,`table_name`), ADD UNIQUE KEY `table_name` (`table_name`), ADD KEY `content_elements_tables_group_idx` (`group_id`), ADD KEY `translate` (`translate`);
+
+--
+-- Indexes for table `#__neno_content_element_translations`
+--
+ALTER TABLE `#__neno_content_element_translations`
+ADD PRIMARY KEY (`id`), ADD KEY `content_id` (`content_id`), ADD KEY `content_type` (`content_type`,`content_id`), ADD KEY `content_type_2` (`content_type`), ADD KEY `language` (`language`), ADD KEY `content_type_3` (`content_type`,`content_id`,`language`), ADD KEY `state` (`state`), ADD KEY `content_type_4` (`content_type`,`content_id`,`language`,`state`), ADD KEY `content_type_5` (`content_type`,`content_id`);
+
+--
+-- Indexes for table `#__neno_settings`
+--
+ALTER TABLE `#__neno_settings`
+ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `setting_key_UNIQUE` (`setting_key`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `#__neno_content_element_fields`
+--
+ALTER TABLE `#__neno_content_element_fields`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `#__neno_content_element_groups`
+--
+ALTER TABLE `#__neno_content_element_groups`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `#__neno_content_element_langstrings`
+--
+ALTER TABLE `#__neno_content_element_langstrings`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `#__neno_content_element_presets`
+--
+ALTER TABLE `#__neno_content_element_presets`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `#__neno_content_element_tables`
+--
+ALTER TABLE `#__neno_content_element_tables`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `#__neno_content_element_translations`
+--
+ALTER TABLE `#__neno_content_element_translations`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `#__neno_settings`
+--
+ALTER TABLE `#__neno_settings`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
@@ -196,7 +253,7 @@ CREATE TABLE IF NOT EXISTS `#__neno_translators` (
 -- Constraints for table `#__neno_content_elements_metadata_x_translators`
 --
 ALTER TABLE `#__neno_content_elements_metadata_x_translators`
-ADD CONSTRAINT `fk_cem_x_translators_content_element_metadata_idx` FOREIGN KEY (`content_element_metadata_id`) REFERENCES `#__neno_content_element_metadata` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_cem_x_translators_content_element_metadata_idx` FOREIGN KEY (`content_element_metadata_id`) REFERENCES `#__neno_content_element_translations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
 ADD CONSTRAINT `fk_cem_x_translators_translator_idx` FOREIGN KEY (`translator_id`) REFERENCES `#__neno_translators` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
@@ -206,34 +263,35 @@ ALTER TABLE `#__neno_content_element_fields`
 ADD CONSTRAINT `fk_cef_table_idx` FOREIGN KEY (`table_id`) REFERENCES `#__neno_content_element_tables` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
+-- Constraints for table `#__neno_content_element_fields_x_translations`
+--
+ALTER TABLE `#__neno_content_element_fields_x_translations`
+ADD CONSTRAINT `fk_#__neno_content_element_fields_has_#__neno_content_element1` FOREIGN KEY (`field_id`) REFERENCES `#__neno_content_element_fields` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_#__neno_content_element_fields_has_#__neno_content_element2` FOREIGN KEY (`translation_id`) REFERENCES `#__neno_content_element_translations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
 -- Constraints for table `#__neno_content_element_groups`
 --
 ALTER TABLE `#__neno_content_element_groups`
 ADD CONSTRAINT `#__neno_content_element_groups_ibfk_1` FOREIGN KEY (`extension_id`) REFERENCES `#__extensions` (`extension_id`);
 
 --
--- Constraints for table `#__neno_content_element_langfile_sources`
+-- Constraints for table `#__neno_content_element_langstrings`
 --
-ALTER TABLE `#__neno_content_element_langfile_sources`
-ADD CONSTRAINT `#__neno_content_element_langfile_sources_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `#__neno_content_element_groups` (`id`);
+ALTER TABLE `#__neno_content_element_langstrings`
+ADD CONSTRAINT `#__neno_content_element_langstrings_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `#__neno_content_element_groups` (`id`);
 
 --
--- Constraints for table `#__neno_content_element_langfile_translations`
+-- Constraints for table `#__neno_content_element_presets`
 --
-ALTER TABLE `#__neno_content_element_langfile_translations`
-ADD CONSTRAINT `fk_lt_source_idx` FOREIGN KEY (`source_id`) REFERENCES `#__neno_content_element_langfile_sources` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `#__neno_content_element_preset`
---
-ALTER TABLE `#__neno_content_element_preset`
+ALTER TABLE `#__neno_content_element_presets`
 ADD CONSTRAINT `fk_cep_group_idx` FOREIGN KEY (`group_id`) REFERENCES `#__neno_content_element_groups` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `#__neno_content_element_preset_x_translators`
 --
 ALTER TABLE `#__neno_content_element_preset_x_translators`
-ADD CONSTRAINT `fk_cep_x_translators_content_element_preset_idx` FOREIGN KEY (`content_element_preset_id`) REFERENCES `#__neno_content_element_preset` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_cep_x_translators_content_element_preset_idx` FOREIGN KEY (`content_element_preset_id`) REFERENCES `#__neno_content_element_presets` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
 ADD CONSTRAINT `fk_cep_x_translators_translator_idx` FOREIGN KEY (`translator_id`) REFERENCES `#__neno_translators` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
