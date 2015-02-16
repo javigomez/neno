905ecf3f1eb00a7637b9a8d82bdf63b45a5ef84d
Sandeep <sandeep@notwebdesign.com>
2015-02-16 19:49:11 +0530

    Implemented method convertFromJisoToIso

--- libraries/neno/translate/api/google.php
+++ libraries/neno/translate/api/google.php
@@ -16,50 +16,89 @@ jimport('joomla.application.component.helper');
  */
 class NenoTranslateApiGoogle extends NenoTranslateApi
 {
-	/**
-	 * @var string TranslateApi
+	 /**
+	 * @var string
 	 */
 	protected $apiKey;
-
+        
 	/**
 	 * Translate text using google api
 	 *
-	 * @param   string $text   text to translate
-	 * @param   string $source source language
-	 * @param   string $target target language
+	 * @param   string $text    text to translate
+	 * @param   string $source  source language default english
+	 * @param   string $target  target language default french
 	 *
 	 * @return string
 	 */
-	public function translate($text, $source = "en", $target = "fr")
+	public function translate($text,$source="en-US",$target="fr-FR")
 	{
-		// Get the key configured by user
+        // get the key configured by user
 		$this->apiKey = JComponentHelper::getParams('com_neno')->get('googleApiKey');
 
-		if ($this->apiKey == "")
-		{
-			// Use default key if not provided
-			$this->apiKey = 'AIzaSyBoWdaSTbZyrRA9RnKZOZZuKeH2l4cdrn8';
-		}
+		// convert from JISO to ISO codes
+		$source = $this->convertFromJisoToIso($source);
+		$target = $this->convertFromJisoToIso($target);
 
-		$url = 'https://www.googleapis.com/language/translate/v2?key=' . $this->apiKey . '&q=' . rawurlencode($text) . '&source=' . $source . '&target=' . $target;
+			if($this->apiKey == "")
+			{
+            	 // Use default key if not provided
+				 $this->apiKey = 'AIzaSyBoWdaSTbZyrRA9RnKZOZZuKeH2l4cdrn8';
+			}
+                
+		$url    = 'https://www.googleapis.com/language/translate/v2?key=' . $this->apiKey . '&q=' . rawurlencode($text) . '&source=' . $source .'&target=' .$target;
 
 		// Invoke the GET request.
 		$response = $this->get($url);
 
 		$text = null;
 
-		// Log it if server response is not OK.
-		if ($response->code != 200)
-		{
-			NenoLog::log('Google api failed with response: ' . $response->code, 1);
-		}
-		else
-		{
-			$responseBody = json_decode($response->body);
-
-			$text = $responseBody->data->translations[0]->translatedText;
-		}
+            // Log it if server response is not OK.
+			if ($response->code != 200)
+			{
+				NenoLog::log('Google api failed with response: ' . $response->code, 1);
+			}
+			else
+			{
+				$responseBody=json_decode($response->body);
+				$text = $responseBody->data->translations[0]->translatedText;
+			}
 
 		return $text;
 	}
+
+	/**
+	 * Method to make supplied language codes equivalent to google api codes
+	 *
+	 * @param   string $jiso Joomla ISO language code
+	 *
+	 * @return string
+	 */
+	public function convertFromJisoToIso($jiso)
+	{
+		// split the language code parts using hypen
+		$jisoParts = (explode("-",$jiso));
+		$iso2Tag = strtolower($jisoParts[0]);
+
+		switch($iso2Tag)
+		{
+			case "zh":
+				$iso2 = $jiso;
+				break;
+
+			case "he":
+				$iso2 = "iw";
+				break;
+
+			case "nb":
+				$iso2 = "no";
+				break;
+
+			default:
+				$iso2 = $iso2Tag;
+				break;
+		}
+
+		return $iso2;
+	}
+
 }
