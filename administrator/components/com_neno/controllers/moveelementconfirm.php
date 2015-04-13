8c1dc60dc83c539c0450ab784041c42d8cf8bf6d
Søren Beck Jensen <soren@notwebdesign.com>
2015-04-13 09:14:15 +0200

    Old groups and elements stuff

--- administrator/components/com_neno/controllers/moveelementconfirm.php
+++ administrator/components/com_neno/controllers/moveelementconfirm.php
@@ -28,12 +28,49 @@ class NenoControllerMoveElementConfirm extends JControllerAdmin
         
         $input = JFactory::getApplication()->input;
         
-        //Overwrite the view
+        // Overwrite the view
         $input->set('view', 'moveelementconfirm');
         
-        //We can ignore groups as they should have all their children checked if they are checked
         $tables = $input->get('tables', array(), 'array');
         $files = $input->get('files', array(), 'files');
+
+        // If a group is selected then load all the elements from that group
+        $groups = $input->get('groups', array(), 'array');
+        if (!empty($groups))
+        {
+            foreach ($groups as $groupId)
+            {
+                $group = NenoContentElementGroup::getGroup($groupId);
+                
+                //Handle tables
+                $group_tables = $group->getTables();
+                if (!empty($group_tables))
+                {
+                    foreach ($group_tables as $group_table)
+                    {
+                        //Add the table id to the tables array
+                        $tables[] = $group_table->getId();
+                    }
+                }
+
+                // Handle files
+                $group_files = $group->getLanguageFiles();
+                if(!empty($group_files))
+                {
+                    foreach ($group_files as $group_file)
+                    {
+                        // Add the file id to the files array
+                        // @todo Get the id when files have id
+                        // $files[] = $group_file->getId();
+                    }
+                }
+
+            }
+        }
+        
+        //Remove duplicates
+        array_unique($tables);
+        array_unique($files);
         
         //Load table info
         if (!empty($tables)) 
