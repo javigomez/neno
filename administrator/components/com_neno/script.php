daaf54eba50cbc87d76178e5bda1f392fd0cc20f
Victor Escobar González <victor@notwebdesign.com>
2015-04-23 17:06:22 +0200

    Updating script.

--- administrator/components/com_neno/script.php
+++ administrator/components/com_neno/script.php
@@ -1,17 +1,46 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: victor
- * Date: 23/04/15
- * Time: 16:39
+ * Installs some files that the installer does not move.
+ *
+ * @since  1.0
  */
 class com_nenoInstallerScript
 {
+	/**
+	 * Copying files
+	 *
+	 * @param string                     $type
+	 * @param JInstallerAdapterComponent $parent
+	 *
+	 * @return bool False if something happens
+	 */
 	public function postflight($type, $parent)
 	{
-		var_dump($type);
-		var_dump($parent);
-		exit;
+		if ($type == 'install' || $type == 'update')
+		{
+			$installationPath = $parent->getParent()->getPath('source');
+
+			jimport('joomla.filesystem.folder');
+
+			// Moving Layouts
+			if (JFolder::move($installationPath . DIRECTORY_SEPARATOR . 'layouts', JPATH_ROOT . DIRECTORY_SEPARATOR . 'layouts' . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'neno') !== true)
+			{
+				return false;
+			}
+
+			// Moving media files
+			if (JFolder::move($installationPath . DIRECTORY_SEPARATOR . 'media', JPATH_ROOT . DIRECTORY_SEPARATOR . 'media' . DIRECTORY_SEPARATOR . 'neno') !== true)
+			{
+				return false;
+			}
+		}
+		elseif ($type == 'uninstall')
+		{
+			JFolder::delete(JPATH_ROOT . DIRECTORY_SEPARATOR . 'layouts' . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'neno');
+			JFolder::delete(JPATH_ROOT . DIRECTORY_SEPARATOR . 'media' . DIRECTORY_SEPARATOR . 'neno');
+		}
+
+		return true;
 	}
 }
