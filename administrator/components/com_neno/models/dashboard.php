6ce3fbee42a4f2bd4620ad70522f2949949d5cc7
Victor Escobar González <victor@notwebdesign.com>
2015-05-04 16:03:51 +0200

    Improving view rendering.

--- administrator/components/com_neno/models/dashboard.php
+++ administrator/components/com_neno/models/dashboard.php
@@ -31,8 +31,50 @@ class NenoModelDashboard extends JModelList
 		$db->setQuery($this->getListQuery());
 
 		$languages = $db->loadObjectListMultiIndex('lang_code');
+		$items     = array ();
 
-		return $languages;
+		foreach ($languages as $language)
+		{
+			$translated      = 0;
+			$queued          = 0;
+			$changed         = 0;
+			$untranslated    = 0;
+			$item            = new stdClass;
+			$item->lang_code = $language[0]->lang_code;
+			$item->published = $language[0]->published;
+			$item->title     = $language[0]->title;
+			$item->image     = $language[0]->image;
+
+			foreach ($language as $internalItem)
+			{
+				switch ($internalItem->state)
+				{
+					case NenoContentElementTranslation::TRANSLATED_STATE:
+						$untranslated = (int) $internalItem->word_count;
+						break;
+					case NenoContentElementTranslation::QUEUED_FOR_BEING_TRANSLATED_STATE:
+						$untranslated = (int) $internalItem->word_count;
+						break;
+					case NenoContentElementTranslation::SOURCE_CHANGED_STATE:
+						$untranslated = (int) $internalItem->word_count;
+						break;
+					case NenoContentElementTranslation::NOT_TRANSLATED_STATE:
+						$untranslated = (int) $internalItem->word_count;
+						break;
+				}
+			}
+
+			$item->wordCount               = new stdClass;
+			$item->wordCount->translated   = $translated;
+			$item->wordCount->queued       = $queued;
+			$item->wordCount->changed      = $changed;
+			$item->wordCount->untranslated = $untranslated;
+			$item->wordCount->total        = $translated + $queued + $changed + $untranslated;
+
+			$items[] = $item;
+		}
+
+		return $items;
 	}
 
 	/**
@@ -44,7 +86,7 @@ class NenoModelDashboard extends JModelList
 	 */
 	protected function getListQuery()
 	{
-		// Create a new query object.
+// Create a new query object.
 		$db    = JFactory::getDbo();
 		$query = parent::getListQuery();
 
--- administrator/components/com_neno/views/dashboard/tmpl/default.php
+++ administrator/components/com_neno/views/dashboard/tmpl/default.php
@@ -42,7 +42,6 @@ $workingLanguage = NenoHelper::getWorkingLanguage();
 		jQuery('.configuration-button').on('click', function () {
 			jQuery(this).siblings('.language-configuration').slideToggle();
 		});
-
 	});
 </script>
 
@@ -55,53 +54,32 @@ $workingLanguage = NenoHelper::getWorkingLanguage();
 	</div>
 	<div id="j-main-container" class="span10">
 		<div class="languages-holder">
-			<?php foreach ($this->items as $language => $item): ?>
+			<?php foreach ($this->items as $item): ?>
 				<div class="language-wrapper">
-					<?php $translated = 0; ?>
-					<?php $queued = 0; ?>
-					<?php $changed = 0; ?>
-					<?php $untranslated = 0; ?>
-					<?php foreach ($item as $internalItem): ?>
-						<?php if ($internalItem->state == NenoContentElementTranslation::TRANSLATED_STATE): ?>
-							<?php $translated = (int) $internalItem->word_count; ?>
-						<?php elseif ($internalItem->state == NenoContentElementTranslation::QUEUED_FOR_BEING_TRANSLATED_STATE): ?>
-							<?php $queued = (int) $internalItem->word_count; ?>
-						<?php elseif ($internalItem->state == NenoContentElementTranslation::SOURCE_CHANGED_STATE): ?>
-							<?php $changed = (int) $internalItem->word_count; ?>
-						<?php elseif ($internalItem->state == NenoContentElementTranslation::NOT_TRANSLATED_STATE): ?>
-							<?php $untranslated = (int) $internalItem->word_count; ?>
-						<?php endif; ?>
-					<?php endforeach; ?>
-					<?php $wordCount = new stdClass; ?>
-					<?php $wordCount->translated = $translated; ?>
-					<?php $wordCount->queued = $queued; ?>
-					<?php $wordCount->changed = $changed; ?>
-					<?php $wordCount->untranslated = $untranslated; ?>
-					<?php $wordCount->total = $translated + $queued + $changed + $untranslated; ?>
 					<h3>
 						<img
-							src="<?php echo JUri::root() . 'media/mod_languages/images/' . $item[0]->image . '.gif'; ?>"/>
-						<?php echo $item[0]->title; ?>
+							src="<?php echo JUri::root() . 'media/mod_languages/images/' . $item->image . '.gif'; ?>"/>
+						<?php echo $item->title; ?>
 					</h3>
-					<?php echo NenoHelper::renderWordCountProgressBar($wordCount, true, true) ?>
+					<?php echo NenoHelper::renderWordCountProgressBar($item->wordCount, true, true) ?>
 					<a class="btn btn-primary"
-					   href="<?php echo JRoute::_('index.php?option=com_neno&task=editor.translate&lang=' . $language) ?>">
+					   href="<?php echo JRoute::_('index.php?option=com_neno&task=editor.translate&lang=' . $item->lang_code) ?>">
 						<?php echo JText::_('COM_NENO_DASHBOARD_TRANSLATE_BUTTON'); ?>
 					</a>
 					<button class="btn configuration-button" type="button">
 						<?php echo JText::_('COM_NENO_DASHBOARD_CONFIGURATION_BUTTON'); ?>
 					</button>
 					<div class="language-configuration">
-						<?php echo JText::sprintf('COM_NENO_DASHBOARD_GROUPS_ELEMENTS_LINK', JRoute::_('index.php?option=com_neno&task=groupselements.changeConfiguration&lang=' . $language)); ?>
+						<?php echo JText::sprintf('COM_NENO_DASHBOARD_GROUPS_ELEMENTS_LINK', JRoute::_('index.php?option=com_neno&task=groupselements.changeConfiguration&lang=' . $item->lang_code)); ?>
 						<fieldset id="jform_published" class="radio btn-group btn-group-yesno">
 							<input type="radio" id="jform_published0" name="jform[published]" value="1"
-								<?php echo ($item[0]->published) ? 'checked="checked"' : ''; ?>
+								<?php echo ($item->published) ? 'checked="checked"' : ''; ?>
 								>
 							<label for="jform_published0" class="btn">
 								<?php echo JText::_('JPUBLISHED'); ?>
 							</label>
 							<input type="radio" id="jform_published1" name="jform[published]" value="0"
-								<?php echo ($item[0]->published) ? '' : 'checked="checked"'; ?>>
+								<?php echo ($item->published) ? '' : 'checked="checked"'; ?>>
 							<label for="jform_published1" class="btn">
 								<?php echo JText::_('JUNPUBLISHED'); ?>
 							</label>
