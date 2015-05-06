bb3b447a5d6e6fb0833f5408567499eac3b78fff
Victor Escobar González <victor@notwebdesign.com>
2015-05-06 16:37:15 +0200

    Show message after installation.

---
+++ layouts/libraries/neno/installationgetstarted.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('_JEXEC') or die;
+
+?>
+
+<h4>Neno was installed successfully</h4>
+
+<div class="btn-wrapper <?php echo $displayData->class; ?>">
+	<p>
+		Congratulations. You have taken the first step to start getting your web site translated. If you like, you can
+		dive right into the <a href="#">documentation</a> but we recommend that you simply click the "get started"
+		button
+		below as it will take you through our setup guide.
+	</p>
+
+	<a href="<?php echo $displayData; ?>" class="btn btn-success">Get Started</a>
+</div>
