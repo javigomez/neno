a6e9168ae551f9a764f9689efeb2eafea2ffbd86
Victor Escobar González <victor@notwebdesign.com>
2015-05-13 13:15:03 +0200

    BUGFIX Adding debug view.

---
+++ administrator/components/com_neno/views/debug/tmpl/default.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package     Neno
+ * @subpackage  Views
+ *
+ * @author      Jensen Technologies S.L. <info@notwebdesign.com>
+ * @copyright   Copyright (C) 2014 Jensen Technologies S.L. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+?>
+<pre><?php echo print_r(NenoHelper::getServerInfo(), true); ?></pre>
--- libraries/neno/helper/helper.php
+++ libraries/neno/helper/helper.php
@@ -2675,6 +2675,58 @@ class NenoHelper
 	}
 
 	/**
+	 * Get information about PHP
+	 *
+	 * @return array
+	 */
+	public static function getServerInfo()
+	{
+		ob_start();
+		phpinfo(11);
+		$phpInfo = array ('phpinfo' => array ());
+
+		if (preg_match_all('#(?:<h2>(?:<a name=".*?">)?(.*?)(?:</a>)?</h2>)|(?:<tr(?: class=".*?")?><t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>)?)?</tr>)#s', ob_get_clean(), $matches, PREG_SET_ORDER))
+		{
+			foreach ($matches as $match)
+			{
+				if (strlen($match[1]))
+				{
+					$phpInfo[$match[1]] = array ();
+				}
+				elseif (isset($match[3]))
+				{
+					$keys1                           = array_keys($phpInfo);
+					$phpInfo[end($keys1)][$match[2]] = isset($match[4]) ? array ($match[3], $match[4]) : $match[3];
+				}
+				else
+				{
+					$keys1                  = array_keys($phpInfo);
+					$phpInfo[end($keys1)][] = $match[2];
+
+				}
+
+			}
+		}
+
+		if (!empty($phpInfo))
+		{
+			foreach ($phpInfo as $name => $section)
+			{
+				foreach ($section as $key => $val)
+				{
+					if (is_numeric($key))
+					{
+						unset($phpInfo[$name][$key]);
+					}
+
+				}
+			}
+		}
+
+		return $phpInfo;
+	}
+
+	/**
 	 * Get a list of menu items associated to the one passed by argument
 	 *
 	 * @param    integer $menuItemId Menu Item id
