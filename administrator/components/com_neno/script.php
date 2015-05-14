838f2bbf8cac30a730eab9ba71086bb62cdeb636
Victor Escobar González <victor@notwebdesign.com>
2015-05-14 13:09:35 +0200

    Delete layouts and media folders if they exist.

--- administrator/components/com_neno/script.php
+++ administrator/components/com_neno/script.php
@@ -21,8 +21,20 @@ class com_NenoInstallerScript
 
 		jimport('joomla.filesystem.folder');
 
+		// If the layout folder exists, let's delete them first
+		if (JFolder::exists(JPATH_ROOT . '/layouts/libraries/neno'))
+		{
+			JFolder::delete(JPATH_ROOT . '/layouts/libraries/neno');
+		}
+
 		// Moving Layouts
 		JFolder::move($installationPath . '/layouts', JPATH_ROOT . '/layouts/libraries/neno');
+
+		// If the media folder exists, let's delete them first
+		if (JFolder::exists(JPATH_ROOT . '/media/neno'))
+		{
+			JFolder::delete(JPATH_ROOT . '/media/neno');
+		}
 
 		// Moving media files
 		JFolder::move($installationPath . '/media', JPATH_ROOT . '/media/neno');
