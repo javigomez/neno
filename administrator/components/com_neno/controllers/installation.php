27f0a7fe6369b8a3555dabde4f67a89b6d3b03f7
Victor Escobar González <victor@notwebdesign.com>
2015-05-18 17:18:41 +0200

    Removing unused method.

--- administrator/components/com_neno/controllers/installation.php
+++ administrator/components/com_neno/controllers/installation.php
@@ -146,68 +146,6 @@ class NenoControllerInstallation extends JControllerAdmin
 	}
 
 	/**
-	 * Get translation method selector
-	 *
-	 * @return void
-	 */
-	public function getTranslationMethodSelector()
-	{
-		$input              = $this->input;
-		$n                  = $input->getInt('n', 0);
-		$selected_methods   = $input->get('selected_methods', array (), 'ARRAY');
-		$placement          = $input->getCmd('placement');
-		$translationMethods = $placement == 'general' ? NenoHelper::getDefaultTranslationMethods() : NenoHelper::loadTranslationMethods();
-		$app                = JFactory::getApplication();
-
-		// Ensure that we know what was selected for the previous selector
-		if (($n > 0 && !isset($selected_methods[$n - 1])) || ($n > 0 && $selected_methods[$n - 1] == 0))
-		{
-			$app->close();
-		}
-
-		// As a safety measure prevent more than 5 selectors and always allow only one more selector than already selected
-		if ($n > 4 || $n > count($selected_methods) + 1)
-		{
-			$app->close();
-		}
-
-		// Reduce the translation methods offered depending on the parents
-		if ($n > 0 && !empty($selected_methods))
-		{
-			$parent_method                   = $selected_methods[$n - 1];
-			$acceptable_follow_up_method_ids = $translationMethods[$parent_method]->acceptable_follow_up_method_ids;
-			$acceptable_follow_up_methods    = explode(',', $acceptable_follow_up_method_ids);
-
-			foreach ($translationMethods as $k => $translation_method)
-			{
-				if (!in_array($k, $acceptable_follow_up_methods))
-				{
-					unset($translationMethods[$k]);
-				}
-			}
-		}
-
-		// If there are no translation methods left then return nothing
-		if (!count($translationMethods))
-		{
-			JFactory::getApplication()->close();
-		}
-
-		// Prepare display data
-		$displayData                                 = array ();
-		$displayData['translation_methods']          = $translationMethods;
-		$displayData['assigned_translation_methods'] = NenoHelper::getTranslationMethods('dropdown');
-		$displayData['n']                            = $n;
-
-		$selectorHTML = JLayoutHelper::render('translationmethodselector', $displayData, JPATH_NENO_LAYOUTS);
-
-		echo $selectorHTML;
-
-		$app->close();
-
-	}
-
-	/**
 	 * Task to finishing setup
 	 *
 	 * @return void
