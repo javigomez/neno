1c4187dc12c811241dd69c36f0e315029043f983
Victor Escobar González <victor@notwebdesign.com>
2015-05-26 18:00:58 +0200

    Changing ordering to create menus.

--- administrator/components/com_neno/controllers/installation.php
+++ administrator/components/com_neno/controllers/installation.php
@@ -293,32 +293,7 @@ class NenoControllerInstallation extends JControllerAdmin
 		while (!$finished)
 		{
 			// Check if the menus have been created
-			if (NenoSettings::get('discovering_step_menu') != 1)
-			{
-				NenoHelper::setSetupState(0, JText::_('COM_NENO_INSTALLATION_MESSAGE_GENERATING_MENUS'));
-				NenoHelper::createMenuStructure();
-				NenoSettings::set('discovering_step_menu', 1);
-
-				// Calculate percent per extension discovered
-				$extensions = $db->quote(NenoHelper::whichExtensionsShouldBeTranslated());
-				$query
-					->clear()
-					->select('COUNT(*)')
-					->from('`#__extensions` AS e')
-					->where(
-						array (
-							'e.type IN (' . implode(',', $extensions) . ')',
-							'e.name NOT LIKE \'com_neno\''
-						)
-					);
-
-				$db->setQuery($query);
-				$extensionToDiscover = (int) $db->loadResult();
-
-				$percentPerExtension = 85 / $extensionToDiscover;
-				NenoSettings::set('percentPerExtension', $percentPerExtension);
-			}
-			elseif (NenoSettings::get('discovering_extensions') != 1) // Check if the extensions have been discovered
+			if (NenoSettings::get('discovering_extensions') != 1) // Check if the extensions have been discovered
 			{
 				// Check if there was a process executing before
 				if (NenoSettings::get('discovering_field') != null)
@@ -445,6 +420,31 @@ class NenoControllerInstallation extends JControllerAdmin
 				$db->execute();
 
 				NenoSettings::set('publishing_plugins', 1);
+			}
+			elseif (NenoSettings::get('discovering_step_menu') != 1)
+			{
+				NenoHelper::setSetupState(0, JText::_('COM_NENO_INSTALLATION_MESSAGE_GENERATING_MENUS'));
+				NenoHelper::createMenuStructure();
+				NenoSettings::set('discovering_step_menu', 1);
+
+				// Calculate percent per extension discovered
+				$extensions = $db->quote(NenoHelper::whichExtensionsShouldBeTranslated());
+				$query
+					->clear()
+					->select('COUNT(*)')
+					->from('`#__extensions` AS e')
+					->where(
+						array (
+							'e.type IN (' . implode(',', $extensions) . ')',
+							'e.name NOT LIKE \'com_neno\''
+						)
+					);
+
+				$db->setQuery($query);
+				$extensionToDiscover = (int) $db->loadResult();
+
+				$percentPerExtension = 85 / $extensionToDiscover;
+				NenoSettings::set('percentPerExtension', $percentPerExtension);
 			}
 			else
 			{
