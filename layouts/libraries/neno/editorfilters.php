c9ee9dc2a9fa3efd5dd5ee0a1529dc205c827c9a
davidvweb <david@notwebdesign.com>
2015-06-12 13:22:25 +0200

    BUGFIX Problem with editor filters width

--- layouts/libraries/neno/editorfilters.php
+++ layouts/libraries/neno/editorfilters.php
@@ -13,10 +13,10 @@ defined('_JEXEC') or die;
 $data = $displayData;
 
 // Receive overridable options
-$data['options'] = !empty($data['options']) ? $data['options'] : array ();
+$data['options'] = !empty($data['options']) ? $data['options'] : array();
 
 // Set some basic options
-$customOptions = array (
+$customOptions = array(
 	'filtersHidden'       => false,
 	'filterButton'        => false,
 	'defaultLimit'        => isset($data['options']['defaultLimit']) ? $data['options']['defaultLimit'] : JFactory::getApplication()->get('list_limit', 20),
@@ -39,31 +39,23 @@ $document->addScript(JUri::root() . '/media/neno/js/editorfilters.js');
 ?>
 
 <div class="js-stools clearfix">
+	<div class="btn-wrapper input-append">
+		<input type="text" name="filter[search]" id="filter_search" value="" class="js-stools-search-string"
+		       placeholder="Search"/>
+					<span class="btn hasTooltip submit-form"
+					      title="<?php echo JHtml::tooltipText('JSEARCH_FILTER_SUBMIT'); ?>">
+						<i class="icon-search"></i>
+					</span>
+	</div>
 	<div class="clearfix">
-		<div class="js-stools-container-bar">
-			<label for="filter_search" class="element-invisible">
-				<?php echo JText::_('JSEARCH_FILTER'); ?>
-			</label>
-
-			<div class="btn-wrapper input-append">
-				<?php echo $filters['filter_search']->input; ?>
-				<button type="submit" class="btn hasTooltip"
-				        title="<?php echo JHtml::tooltipText('JSEARCH_FILTER_SUBMIT'); ?>">
-					<i class="icon-search"></i>
-				</button>
-			</div>
-		</div>
-		<div class="js-stools-container-list hidden-phone hidden-tablet">
-			<?php echo JLayoutHelper::render('joomla.searchtools.default.list', $data); ?>
-		</div>
 	</div>
 	<!-- Filters div -->
 	<div class="js-stools-container-filters hidden-phone clearfix">
 		<div class="multiselect-wrapper">
-			<?php echo JLayoutHelper::render('simplemultiselect', array ('type' => 'method', 'data' => $data['extraDisplayData']->methods, 'selected' => $data['extraDisplayData']->modelState->get('filter.translator_type')), JPATH_NENO_LAYOUTS); ?>
+			<?php echo JLayoutHelper::render('simplemultiselect', array('type' => 'method', 'data' => $data['extraDisplayData']->methods, 'selected' => $data['extraDisplayData']->modelState->get('filter.translator_type')), JPATH_NENO_LAYOUTS); ?>
 		</div>
 		<div class="multiselect-wrapper">
-			<?php echo JLayoutHelper::render('simplemultiselect', array ('type' => 'status', 'data' => $data['extraDisplayData']->statuses, 'selected' => $data['extraDisplayData']->modelState->get('filter.translation_status')), JPATH_NENO_LAYOUTS); ?>
+			<?php echo JLayoutHelper::render('simplemultiselect', array('type' => 'status', 'data' => $data['extraDisplayData']->statuses, 'selected' => $data['extraDisplayData']->modelState->get('filter.translation_status')), JPATH_NENO_LAYOUTS); ?>
 		</div>
 		<div class="multiselect-wrapper">
 			<?php echo JLayoutHelper::render('multiselectgroup', $data['extraDisplayData'], JPATH_NENO_LAYOUTS); ?>
