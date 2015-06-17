ab43d153b8d5e767fa70345ffb80be97c735077f
Victor Escobar González <victor@notwebdesign.com>
2015-06-17 17:11:28 +0200

    BUGFIX Adding tests.

---
+++ tests/acceptance/InstallNenoCest.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 17/06/15
+ * Time: 16:49
+ */
+class InstallNenoCest
+{
+	public function installNeno(AcceptanceTester $I)
+	{
+		$I->am('Administrator');
+		$I->installJoomla();
+		$I->doAdministratorLogin();
+		$I->setErrorReportingToDevelopment();
+	}
+}
