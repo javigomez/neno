a58a8c56afb88efcae1863f9b8bc0cba599d3659
Victor Escobar González <victor@notwebdesign.com>
2015-06-22 09:32:58 +0200

    Adding some test functionality

--- tests/_support/NenoExtension.php
+++ tests/_support/NenoExtension.php
@@ -19,7 +19,36 @@ class NenoExtension extends \Codeception\Platform\Extension
 
 	public function testFailed(\Codeception\Event\FailEvent $e)
 	{
-		echo "HOLA";
-		print_r(realpath(dirname(__FILE__) . '../_output'));
+		$mail = new PHPMailer;
+
+		$mail->isSMTP();                                      // Set mailer to use SMTP
+		$mail->Host       = 'smtp.mandrillapp.com';  // Specify main and backup SMTP servers
+		$mail->SMTPAuth   = true;                               // Enable SMTP authentication
+		$mail->Username   = 'info@notwebdesign.com';                 // SMTP username
+		$mail->Password   = 'i8YaATdXRFRp56C975n_xA';                           // SMTP password
+		$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+		$mail->Port       = 587;                                    // TCP port to connect to
+
+		$mail->From     = 'info@neno-translate.com';
+		$mail->FromName = 'Neno Travis';
+		$mail->addAddress('victor@notwebdesign.com');
+
+		/*$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+		$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+		$mail->isHTML(true);                                  // Set email format to HTML*/
+
+		$mail->Subject = 'Here is the subject';
+		$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+		$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+		if (!$mail->send())
+		{
+			echo 'Message could not be sent.';
+			echo 'Mailer Error: ' . $mail->ErrorInfo;
+		}
+		else
+		{
+			echo 'Message has been sent';
+		}
 	}
 }
