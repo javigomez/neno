28f24ea853762f90607a129550ad62f53f52f612
Victor Escobar González <victor@notwebdesign.com>
2015-06-22 11:41:20 +0200

    Adding cloudinary and github support.

--- tests/_support/NenoExtension.php
+++ tests/_support/NenoExtension.php
@@ -21,37 +21,26 @@ class NenoExtension extends \Codeception\Platform\Extension
 
 	public function testFailed(\Codeception\Event\FailEvent $e)
 	{
-		$mail = new PHPMailer;
+		// Upload image
+		Cloudinary::config(
+			array (
+				'cloud_name' => $this->config['cloud_name'],
+				'api_key'    => $this->config['api_key'],
+				'api_secret' => $this->config['api_secret']
+			)
+		);
 
-		$mail->isSMTP();                                      // Set mailer to use SMTP
-		$mail->Host       = 'smtp.mandrillapp.com';  // Specify main and backup SMTP servers
-		$mail->SMTPAuth   = true;                               // Enable SMTP authentication
-		$mail->Username   = 'info@notwebdesign.com';                 // SMTP username
-		$mail->Password   = 'i8YaATdXRFRp56C975n_xA';                           // SMTP password
-		$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-		$mail->Port       = 587;                                    // TCP port to connect to
+		$result = \Cloudinary\Uploader::upload(realpath(dirname(__FILE__) . '/../_output/InstallNenoCest.installNeno.fail.png'));
 
-		$mail->From     = 'info@neno-translate.com';
-		$mail->FromName = 'Neno Travis';
-		$mail->addAddress('victor@notwebdesign.com');
 
-		/*$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-		$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-		$mail->isHTML(true);                                  // Set email format to HTML*/
+		// Create Github issue
+		$client = new \Github\Client();
+		$token  = $this->config['token'];
 
-		file_put_contents(dirname(__FILE__) . '/log.txt', $e->getFail()->getMessage());
+		$client->authenticate($token, \Github\Client::AUTH_URL_TOKEN);
 
-		$mail->Subject = 'Here is the subject';
-		$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-		$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-		if (!$mail->send())
-		{
-			file_put_contents(dirname(__FILE__) . '/log.txt', $mail->ErrorInfo);
-		}
-		else
-		{
-			file_put_contents(dirname(__FILE__) . '/log.txt', 'Email sent');
-		}
+		$client
+			->api('issue')
+			->create('Jensen-Technologies', 'neno', array ('title' => 'Issue testing Neno', 'body' => $result['secure_url']));
 	}
 }
