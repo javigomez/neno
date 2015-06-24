8afc42b2038492fb76aa3dcb03fe9213c7238e87
Victor Escobar González <victor@notwebdesign.com>
2015-06-24 16:50:09 +0200

    The test doesn't wait.

--- tests/acceptance/InstallLanguageCest.php
+++ tests/acceptance/InstallLanguageCest.php
@@ -8,7 +8,9 @@ class InstallLanguageCest
 		$I->doAdministratorLogin();
 		$I->click("Components");
 		$I->click("Neno");
+		$I->waitForElement('#add-languages-button', 10);
 		$I->click("#add-languages-button");
+		$I->waitForElementVisible("div.modal-footer");
 		$I->click("(//button[@type='button'])[77]");
 		$I->click("div.modal-footer");
 		$I->click("Close");
