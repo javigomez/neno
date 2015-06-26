342b88744fcc2de8ef66aba6ab062281c23079fd
Victor Escobar González <victor@notwebdesign.com>
2015-06-26 08:54:55 +0200

    BUGFIX Go back to the previous state of tests.

--- tests/acceptance/01-InstallNenoCest.php
+++ tests/acceptance/01-InstallNenoCest.php
@@ -19,29 +19,64 @@ class InstallNenoCest
 		$I->click("Extension Manager");
 		$I->click("Upload Package File");
 		$path = $I->getConfiguration('repo_folder');
+
+		// Installing library
 		$I->installExtensionFromDirectory($path . 'lib_neno');
+
+		// Installing Plugin
 		$I->installExtensionFromDirectory($path . 'plg_system_neno');
+
+		// Installing Component
 		$I->installExtensionFromDirectory($path . 'com_neno');
-		$I->enablePlugin('Neno');
+
+		/*
+
+		// Enabling plugin
+		$I->enablePlugin('Neno plugin');
+
+		// Going to Neno
 		$I->click("Components");
 		$I->click("Neno");
-		$I->waitForElement(['xpath' => "//button[@type='button']"], 10);
+		$I->wait(5);
+
+		// Get started Screen
+		$I->click('Get Started');
+		$I->waitForJS('return jQuery.active == 0', 20);
+		$I->wait(5);
+
+		// First step - Source language
+		$I->see('Next', 'button');
 		$I->click(['xpath' => "//button[@type='button']"]);
-		$I->waitForElement(['xpath' => "//button[@type='button']"], 10);
+		$I->waitForJS('return jQuery.active == 0', 20);
+		$I->wait(5);
+
+		// Second step - Translation methods
+		$I->see('Next');
 		$I->click(['xpath' => "//button[@type='button']"]);
-		$I->waitForElement(['xpath' => "//button[@type='button']"], 10);
-		$I->click(['xpath' => "//button[@type='button']"]);
-		$I->waitForElement(['css' => "#add-languages-button"], 10);
+		$I->waitForJS('return jQuery.active == 0', 20);
+		$I->wait(5);
+
+		// Third step- Install language(s)
 		$I->click(['css' => "#add-languages-button"]);
+		$I->waitForJS('return jQuery.active == 0', 20);
+		$I->wait(5);
 		$I->waitForElementVisible(['css' => '#languages-modal']);
-		$I->click(['xpath' => "(//button[@type='button'])[29]"]);
-		$I->click(['xpath' => "(//button[@type='button'])[34]"]);
+		$I->click(['css' => "[data-language='de-DE']"]);
+		$I->click(['css' => "[data-language='es-ES']"]);
+		$I->wait(10);
+		$I->seeElement(['css' => ".loading-iso-de-DE"]);
+		$I->seeElement(['css' => ".loading-iso-es-ES"]);
 		$I->click("Close");
 		$I->click(['xpath' => "(//button[@type='button'])[4]"]);
+
+		// Fourth step- Installing Neno
+		$I->wait(5);
 		$I->click(['css' => "#backup-created-checkbox"]);
 		$I->click(['css' => "label.checkbox"]);
 		$I->click(['css' => "#proceed-button"]);
+
+		// Fifth step- Installing Neno has been accomplish successfully
 		$I->waitForElement("#submenu > li > a");
-		$I->doAdministratorLogout();
+		$I->doAdministratorLogout();*/
 	}
 }
